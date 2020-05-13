4f3d974f083a3a6c933f336b5ef3451e682f41f2
Aalegre Desktop <alberto.alegre@enti.cat>
2020-05-13 20:52:59 +0200

    Ruiditos

--- code/src/physics.cpp
+++ code/src/physics.cpp
@@ -12,6 +12,13 @@
 #include <string>
 #include <vector>
 #include <time.h>
+#include <cmath>
+#if __has_include("FastNoise.h")
+#  include "FastNoise.h"
+#  define PERLIN 1
+#else
+#  define PERLIN 0
+#endif
 
 namespace Sphere {
 	extern void setupSphere(glm::vec3 pos, float radius);
@@ -61,6 +68,58 @@ public:
 	}
 };
 
+struct Wave {
+	float amplitude;
+	float frequency;// ω
+	glm::vec3 vector;
+	float length;
+	float lambda;// λ
+	float phase;// Φ
+	Wave() {
+		amplitude = .2f;
+		frequency = 2.5f;
+		vector = { 1,0,1 };
+		length = 2.5f;
+		lambda = 1.f;
+		phase = glm::half_pi<float>();
+	}
+};
+
+#if PERLIN
+struct Noise {
+	FastNoise noise;
+	float frequency = 1;
+	int octaves = 8;
+	glm::vec3 speed = { 1,0,1 };
+	glm::vec3 influence = { .1f,.1f,.1f };
+	float scale = 1;
+	glm::vec3 position = { 0,0,0 };
+	void NextFrame(float dt) {
+		position += speed * dt;
+	}
+	float GetValue(glm::vec3 pos, float range = 1) {
+		return noise.GetNoise((position.x + pos.x) * scale, (position.y + pos.y) * scale, (position.z + pos.z) * scale) * (range * 2) - range;
+	}
+};
+#endif
+
+struct WaveType {
+	std::string name = "Custom";
+	std::vector<Wave> waves;
+	WaveType() {
+		Wave tempWave;
+		tempWave.vector = { -1,0,1 };
+		waves.push_back(tempWave);
+		tempWave.vector = { 1,0,1 };
+		waves.push_back(tempWave);
+	}
+#if PERLIN
+	glm::vec3 speed = { 0,0,0 };
+	glm::vec3 influence = { 0,0,0 };
+	float scale = 0;
+#endif
+};
+
 class Fluid {
 public:
 
@@ -75,12 +134,12 @@ public:
 	glm::vec3* positions;
 	glm::vec3* originalPositions;
 	// Totes les fórmules
-	std::vector<float> amplitudes;
-	std::vector<float> frequencies; // ω
-	std::vector <glm::vec3> waveVectors;
-	std::vector <float> waveLengths;
-	std::vector <float> lambdas; // λ
-	std::vector <float> phases; // Φ
+	std::vector<WaveType> waveTypes;
+	std::vector<Wave> waves;
+#if PERLIN
+	Noise rand;
+#endif
+
 	float totalTime = 0;
 
 
@@ -99,19 +158,14 @@ public:
 	void Init()
 	{
 		// Inicialitzem una fórmula per ara
-		amplitudes.push_back(0.2f);
-		frequencies.push_back(2.5f);
-		waveLengths.push_back(2.5f);
-		lambdas.push_back(1.0f);
-		phases.push_back(glm::half_pi<float>()); // pi/2, per exemple
-		waveVectors.push_back({ 1,0,1 }); // Només en eix X i Z
-		amplitudes.push_back(0.2f);
+		waveTypes.push_back(WaveType());
+		SetWaveType(WaveType());
 
-		frequencies.push_back(2.5f);
-		waveLengths.push_back(2.5f);
-		lambdas.push_back(1.0f);
-		phases.push_back(glm::half_pi<float>()); // pi/2, per exemple
-		waveVectors.push_back({ -1,0,1 }); // Només en eix X i Z
+#if PERLIN
+		rand.noise.SetNoiseType(FastNoise::NoiseType::Value);
+		rand.noise.SetFrequency(rand.frequency);
+		rand.noise.SetInterp(FastNoise::Interp::Quintic);
+#endif
 
 		ClothMesh::setupClothMesh();
 		positions = new glm::vec3[RESOLUTION];
@@ -135,29 +189,46 @@ public:
 			x = x0 - (k/Ḵ)Asin(k * x0 - ωt); // aixó es una posició
 			y = A cos(k * x0-wt);
 			Ḵ = 2π/λ
-			//// 
+			////
 			x es un vector, ya nos indica la X y la Z
 			x = x0 - ∑((ki/Ḵi)*Ai sin(ki * x0 - ωit + Φi);
 			y = ∑(Ai cos(ki * x0 - ωit + Φi);
 			Φ = fase(inventado)
 		*/
+#if PERLIN
+		rand.NextFrame(dt);
+#endif
 		for (int i = 0; i < RESOLUTION; i++)
 		{
 
-			glm::vec3 tmpVec = glm::vec3(0.0f,0.0f,0.0f);
+			glm::vec3 tmpVec = glm::vec3(0.0f, 0.0f, 0.0f);
 			float tmpY = originalPositions[i].y;
-			for (int j = 0; j < lambdas.size(); j++) // J = onda actual
+			for (int j = 0; j < waves.size(); j++) // J = onda actual
 			{
-				float waveLength = glm::length(waveVectors[j]);
+
+				//std::cout << randomFactor << std::endl;
+
 				// std::cout << waveLength * amplitudes[j] << std::endl; // Si la onda hace cosas raras es qüe la amplitüd es demasiado o el waveVec tambien, testear aquí
-				float tmpSin = glm::dot(waveVectors[j], originalPositions[i]) - (frequencies[j] * totalTime) + phases[j];
-				tmpVec += (waveVectors[j]/waveLength)  * amplitudes[j] * glm::sin(tmpSin);
-				
-				float tmpCos = glm::dot(waveVectors[j], originalPositions[i]) - (frequencies[j] * totalTime) + phases[j];
-				tmpY += amplitudes[j] * glm::cos(tmpCos);
+				float tmpSin = glm::dot(waves[j].vector, originalPositions[i]) - (waves[j].frequency * totalTime) + waves[j].phase;
+				tmpVec += ((waves[j].vector / glm::length(waves[j].length)) * waves[j].amplitude * glm::sin(tmpSin));
+
+				float tmpCos = glm::dot(waves[j].vector, originalPositions[i]) - (waves[j].frequency * totalTime) + waves[j].phase;
+				tmpY += (waves[j].amplitude * glm::cos(tmpCos));
 			}
 			tmpVec = originalPositions[i] - tmpVec;
-			positions[i] = {tmpVec.x, tmpY, tmpVec.z};
+			glm::vec3 newPos = { tmpVec.x, tmpY, tmpVec.z };
+#if PERLIN
+			if (rand.influence.x > 0) {
+				newPos.x += rand.GetValue(newPos, rand.influence.x);
+			}
+			if (rand.influence.y > 0) {
+				newPos.y += rand.GetValue(newPos, rand.influence.y);
+			}
+			if (rand.influence.z > 0) {
+				newPos.z += rand.GetValue(newPos, rand.influence.z);
+			}
+#endif
+			positions[i] = newPos;
 		}
 		totalTime += dt;
 
@@ -169,6 +240,15 @@ public:
 		delete[] originalPositions;
 		delete[] positions;
 		ClothMesh::cleanupClothMesh();
+	}
+
+	void SetWaveType(WaveType wt) {
+		waves = wt.waves;
+#if PERLIN
+		rand.speed = wt.speed;
+		rand.influence = wt.influence;
+		rand.scale = wt.scale;
+#endif
 	}
 };
 
@@ -185,6 +265,50 @@ void GUI() {
 		if (simulate) {
 			ImGui::SliderFloat("Simulation speed", &simulationSpeed, 0, 2);
 		}
+
+		ImGui::Spacing();
+		ImGui::Text("Fluid:");
+		ImGui::Spacing();
+#if PERLIN
+		ImGui::DragFloat3("Noise influence", &fluid.rand.influence[0], .01f);
+		ImGui::DragFloat3("Noise speed", &fluid.rand.speed[0], .01f);
+		ImGui::DragFloat("Noise scale", &fluid.rand.scale, .01f);
+		ImGui::Spacing();
+#endif
+		int newWaveCount = fluid.waves.size();
+		ImGui::DragInt("Wave count", &newWaveCount, .1f);
+		for (size_t i = 0; i < fluid.waves.size(); i++)
+		{
+			ImGui::Spacing();
+			std::string tempString = std::to_string(i + 1);
+			tempString += " Amplitude";
+			ImGui::DragFloat(tempString.c_str(), &fluid.waves[i].amplitude, .01f);
+			tempString = std::to_string(i + 1);
+			tempString += " Frequency";
+			ImGui::DragFloat(tempString.c_str(), &fluid.waves[i].frequency, .01f);
+			tempString = std::to_string(i + 1);
+			tempString += " Lambda";
+			ImGui::DragFloat(tempString.c_str(), &fluid.waves[i].lambda, .01f);
+			tempString = std::to_string(i + 1);
+			tempString += " Length";
+			ImGui::DragFloat(tempString.c_str(), &fluid.waves[i].length, .01f);
+			tempString = std::to_string(i + 1);
+			tempString += " Phase";
+			ImGui::DragFloat(tempString.c_str(), &fluid.waves[i].phase, .01f);
+			tempString = std::to_string(i + 1);
+			tempString += " Vector";
+			ImGui::DragFloat3(tempString.c_str(), &fluid.waves[i].vector[0], .01f);
+		}
+		if (newWaveCount < 0)
+			newWaveCount = 0;
+		if (newWaveCount != fluid.waves.size()) {
+			while (newWaveCount > fluid.waves.size()) {
+				fluid.waves.push_back(Wave());
+			}
+			while (newWaveCount < fluid.waves.size()) {
+				fluid.waves.pop_back();
+			}
+		}
 	}
 
 	ImGui::End();
@@ -196,6 +320,7 @@ void PhysicsInit() {
 }
 
 void PhysicsUpdate(float dt) {
+	srand(time(NULL));
 	if (simulate) {
 		fluid.Update(dt * simulationSpeed);
 		ball.Update(dt * simulationSpeed);
